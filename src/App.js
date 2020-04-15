c62bfd3bb1253b94d97218d94605166cca46f1c9
DURAISAMY RAMESH <ramesh.duraisamy@decathlon.com>
2020-04-15 09:23:20 +0530

    facility calender design fix

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ const App = () => {
           />
         </div>
       )}
-      <div>
+      <div className="center-calendar">
         <FacilityCalendar />
         {/* <NewCalendar /> */}
       </div>
