ff6abde98c198be99744a8d23579db851dc40a58
unknown <gaurav.v@appliedsni.com>
2020-04-29 17:39:10 +0530

    modal

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
-import React, { useState } from "react";
-import Calendar from "react-calendar";
+import React from "react";
+import Modal from "react-modal";
 
-import "react-calendar/dist/Calendar.css";
-import MonthView from "react-calendar/dist/umd/MonthView";
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
 
 function App() {
-  const [date, setDate] = useState("");
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+  function openModal() {
+    setIsOpen(true);
+  }
 
-  const onChange = (date) => setDate(date);
+  function closeModal() {
+    setIsOpen(false);
+  }
 
   return (
     <div>
-      <Calendar onChange={onChange} value={date} view="year" />
+      <button onClick={openModal}>Open Modal</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <h2>Hello</h2>
+        <button onClick={closeModal}>close</button>
+        <div>I am a modal</div>
+      </Modal>
     </div>
   );
 }
-
 export default App;
